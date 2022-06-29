9fe28359ffc69058cc2ab5cad7aee30cc825e703
Simul Bista <simulbista@gmail.com>
2022-06-29 16:38:17 -0400

    quiz2 xml and js - array functions-map,filter and reduce

---
+++ quiz2/data.js
@@ -0,0 +1,110 @@
+const data = [
+    {
+      "isActive": false,
+      "balance": 1343.69,
+      "name": "Hobbs Macdonald",
+      "registered": "2015-02-08T02:36:36 +05:00",
+      "latitude": -80.356177,
+      "longitude": -125.276756,
+      "friends": [
+        {
+          "id": 0,
+          "name": "Hart Guthrie"
+        },
+        {
+          "id": 1,
+          "name": "Kasey Gomez"
+        }
+      ]
+    },
+    {
+      "isActive": false,
+      "balance": 2426.98,
+      "name": "Nelda Sykes",
+      "registered": "2015-02-01T10:16:00 +05:00",
+      "latitude": 72.078718,
+      "longitude": -159.71397,
+      "friends": [
+        {
+          "id": 0,
+          "name": "Sullivan Preston"
+        },
+        {
+          "id": 1,
+          "name": "Mcgee James"
+        }
+      ]
+    },
+    {
+      "isActive": true,
+      "balance": 1257.12,
+      "name": "Shaw Walls",
+      "registered": "2018-08-26T02:26:15 +04:00",
+      "latitude": -76.647851,
+      "longitude": 173.881151,
+      "friends": [
+        {
+          "id": 0,
+          "name": "Reyna Wilkins"
+        },
+        {
+          "id": 1,
+          "name": "Deann Christensen"
+        }
+      ]
+    },
+    {
+      "isActive": true,
+      "balance": 1431.44,
+      "name": "Boyer Riley",
+      "registered": "2018-09-24T10:12:16 +04:00",
+      "latitude": -60.360275,
+      "longitude": 19.826812,
+      "friends": [
+        {
+          "id": 0,
+          "name": "Steele Coleman"
+        },
+        {
+          "id": 1,
+          "name": "Darcy Dixon"
+        }
+      ]
+    },
+    {
+      "isActive": false,
+      "balance": 3038.94,
+      "name": "George Snider",
+      "registered": "2017-09-25T09:12:43 +04:00",
+      "latitude": -67.814834,
+      "longitude": -145.045949,
+      "friends": [
+        {
+          "id": 0,
+          "name": "Irene Rivers"
+        },
+        {
+          "id": 1,
+          "name": "Dora Hart"
+        }
+      ]
+    }
+  ];
+
+// displaying the active accounts
+console.log("The active accounts are: ");
+const activeSet = data.filter((record) => record.isActive);
+// console.log(activeSet);
+
+//displaying the highest balance
+const highestBalance = data.reduce((acc,value) =>acc > value.balance? acc:value.balance);
+// console.log("The highest balance is  " + highestBalance);
+
+//get array of friends from all accounts
+console.log("The array of friends is : ");
+const arrOfFriends = data.flatMap((record) => (record.friends)).map((item) => item.name);
+// console.log(arrOfFriends);
+
+//get account holder names separated by commas
+const accHolderNames = data.map((record) => record.name).reduce((acc,value) => acc + ', ' + value);
+console.log("The account holders are : " + accHolderNames);
---
+++ quiz2/data1.js
@@ -0,0 +1,172 @@
+const data1 = [
+    {
+        "totalPages" : 11898,
+        "totalPassengers" : 118972,
+        "data":[
+            {
+                "name" : "John Doe",
+                "trips" : 250,
+                "airline":[
+                    {
+                        "country" : "Taiwan",
+                        "established" : 1989,
+                        "head_quaters" : "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
+                        "id" : 5,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
+                        "name" : "Eva Air",
+                        "slogan" : "Sharing the World, Flying Together",
+                        "website" : "www.evaair.com"
+                    }
+                ]
+            },
+            {
+                "name" : "John Doe",
+                "trips" : 250,
+                "airline":[
+                    {
+                        "country" : "Taiwan",
+                        "established" : 1989,
+                        "head_quaters" : "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
+                        "id" : 5,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
+                        "name" : "Eva Air",
+                        "slogan" : "Sharing the World, Flying Together",
+                        "website" : "www.evaair.com"
+                    }
+                ]
+            },
+            {
+                "airline":[
+                    {
+                        "country" : "Taiwan",
+                        "established" : 1989,
+                        "head_quaters" : "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
+                        "id" : 5,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
+                        "name" : "Eva Air",
+                        "slogan" : "Sharing the World, Flying Together",
+                        "website" : "www.evaair.com"
+                    }
+                ]
+            },
+            {
+                "name" : "John Doe",
+                "trips" : 250,
+                "airline":[
+                    {
+                        "country" : "Taiwan",
+                        "established" : 1989,
+                        "head_quaters" : "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
+                        "id" : 5,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
+                        "name" : "Eva Air",
+                        "slogan" : "Sharing the World, Flying Together",
+                        "website" : "www.evaair.com"
+                    }
+                ]
+            },
+            {
+                "name" : "John Doe",
+                "trips" : 250,
+                "airline":[
+                    {
+                        "country" : "Taiwan",
+                        "established" : 1989,
+                        "head_quaters" : "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
+                        "id" : 5,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
+                        "name" : "Eva Air",
+                        "slogan" : "Sharing the World, Flying Together",
+                        "website" : "www.evaair.com"
+                    }
+                ]
+            }, 
+            {
+                "name" : "John Doe",
+                "trips" : 250,
+                "airline":[
+                    {
+                        "country" : "Taiwan",
+                        "established" : 1989,
+                        "head_quaters" : "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
+                        "id" : 5,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
+                        "name" : "Eva Air",
+                        "slogan" : "Sharing the World, Flying Together",
+                        "website" : "www.evaair.com"
+                    }
+                ]
+            },
+            {
+                "name" : "John Doe",
+                "trips" : 250,
+                "airline":[
+                    {
+                        "country" : "Taiwan",
+                        "established" : 1989,
+                        "head_quaters" : "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
+                        "id" : 5,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
+                        "name" : "Eva Air",
+                        "slogan" : "Sharing the World, Flying Together",
+                        "website" : "www.evaair.com"
+                    }
+                ]
+            },
+            {
+                "name" : "Muhammad",
+                "trips" : 2,
+                "airline":[
+                    {
+                        "country" : "Singapore",
+                        "established" : 1947,
+                        "head_quaters" : "Airline House, 25 Airline Road, Singapore 819829",
+                        "id" : 2,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Singapore_Airlines_Logo_2.svg/250px-Singapore_Airlines_Logo_2.svg.png",
+                        "name" : "Singapore Airlines",
+                        "slogan" : "A Great Way to Fly",
+                        "website" : "www.singaporeair.com"
+                    }
+                ]
+            },
+            {
+                "name" : "Muhammad",
+                "trips" : 2,
+                "airline":[
+                    {
+                        "country" : "Singapore",
+                        "established" : 1947,
+                        "head_quaters" : "Airline House, 25 Airline Road, Singapore 819829",
+                        "id" : 2,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Singapore_Airlines_Logo_2.svg/250px-Singapore_Airlines_Logo_2.svg.png",
+                        "name" : "Singapore Airlines",
+                        "slogan" : "A Great Way to Fly",
+                        "website" : "www.singaporeair.com"
+                    }
+                ]
+            },
+            {
+                "name" : "Hussain",
+                "trips" : 100,
+                "airline":[
+                    {
+                        "country" : "Singapore",
+                        "established" : 1947,
+                        "head_quaters" : "Airline House, 25 Airline Road, Singapore 819829",
+                        "id" : 2,
+                        "logo" : "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Singapore_Airlines_Logo_2.svg/250px-Singapore_Airlines_Logo_2.svg.png",
+                        "name" : "Singapore Airlines",
+                        "slogan" : "A Great Way to Fly",
+                        "website" : "www.singaporeair.com"
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+//total count of all trips made by all the passengers
+console.log("Total trip count made by all passengers is ");
+const totalCount = data1.flatMap((record) => record.data).map((item) => 
+item.trips).filter((value)=>value).reduce((acc,value) => acc+value);
+console.log(totalCount);
