29ae2becd507313778042a5fd8512bd2ebe626bb
Simul Bista <simulbista@gmail.com>
2022-08-05 17:18:53 -0400

    project-2 second commit

--- project-2/app.js
+++ project-2/app.js
@@ -4,23 +4,29 @@ const YOUR_ACCESS_KEY = `4a71749bd71a07c6ee3f2a14b13ff58b`;
 const getNews = async () => {
     const result = await fetch(`http://api.mediastack.com/v1/news?access_key=${YOUR_ACCESS_KEY}`);
     const data = await result.json();
-    // console.log(data);
+    console.log(data);
     return data;
   };
+
+  //returns the full name of the country by passing the abbreviation
+  const getFullCountry = (key) =>{
+        if(key==='eg') return "Egypt"; 
+        if(key==='ma') return "Morocco";
+        if(key==='iq') return "Iraq"; 
+  }
 
 const loadNews = async() => {
     const news = await getNews();
     const list = document.getElementById(`news`);
-    // console.log(news.data);
-
-    news.data.map(({source,category,country,image,url}) => {
+    // filtering out news data which have null values
+    filteredNews = news.data.filter((data) => data.image);
+    // console.log(filteredNews);
+    filteredNews.map(({title,category,country,image,url}) => {
         const html = `
             <article>
-                <h2>${source}</h2>
-                <h2>${category}</h2>
-                <h2>${country}</h2>
-                <h2>${url}</h2>
-                <h3>......<h3>
+                <h1>Title: ${title.substring(0, 35)}...</h2>
+                <h4>Category: ${category} Country: ${getFullCountry(country)}</h4>
+                <a href="${url}" target="_blank"><img src="${image}" alt="news_title_image" style="width:150px;height:150px;"></a>
             </article>
         `;
 
